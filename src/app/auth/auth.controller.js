412a1b267a7319ebe7547572a6b8c173c90da44c
irasema <selmadelgado@gmail.com>
2016-07-15 20:44:54 -0500

    Connect Registration Page to Firebase

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -4,9 +4,27 @@
    angular
      .module('app.auth')
      .controller('AuthController', AuthController);
+    
+    AuthController.$inject = ['$firebaseAuth'];
  
     
-   function AuthController() {     
+   function AuthController($firebaseAuth) {
+       var vm = this;
+       var firebaseAuthObject = $firebaseAuth();
+       
+       vm.user = {
+        email: '',
+        password: ''  
+       };
+       
+       vm.register = register;
+       
+       function register(user) {
+        return firebaseAuthObject.$createUserWithEmailAndPassword(user.email, user.password);
+
+       }
    }
   
 })(); 
+
+
