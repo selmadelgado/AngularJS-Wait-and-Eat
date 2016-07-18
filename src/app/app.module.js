d447e79e52e371572507a90d2a418d8f061a5d2f
irasema <selmadelgado@gmail.com>
2016-07-17 20:40:48 -0500

    Add Route Provider to Catch Unknown URLS

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -16,6 +16,26 @@
     'app.layout',
     'app.waitList'
     
-    ]);
+    ])
+    .config(configFunction)
+    .run(runFunction);
+    
+    configFunction.$inject = ['$routeProvider'];
+    
+    function configFunction($routeProvider) {
+        $routeProvider.otherwise({
+           redirectTo:'/' 
+        });
+    }
+    
+    runFunction.$inject = ['$rootScope', '$location'];
+    
+    function runFunction($rootScope, $location){
+        $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+           if (error === "AUTH_REQUIRED") {
+               $location.path('/');
+           }
+        });
+    }
     
 })();
